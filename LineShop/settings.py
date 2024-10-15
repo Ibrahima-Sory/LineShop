a44e76f09173e9f9e9cf5ce57e6ec8b739aa6303
sory <isbfaka2@gmail.com>
2024-10-15 16:20:47 +0000

    Ajout d'un .env et modification  dans settings

--- LineShop/settings.py
+++ LineShop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tejpkyo5kzh3%m_2$u525aiq_9b06uw5o+kyanl40dh_6p=s#5'
+SECRET_KEY =config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -81,13 +82,14 @@ WSGI_APPLICATION = 'LineShop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'LineShop',
-        'USER': 'sory',
-        'PASSWORD': '14595',
-        'HOST': 'localhost',  # Ou l'adresse IP de votre serveur PostgreSQL
-        'PORT': '5432',       # Le port par défaut pour PostgreSQL
+        'NAME': config('DB_NAME'),
+        'USER':  config('DB_USER') ,
+        'PASSWORD': config('DB_PASSWORD') ,
+        'HOST':  config('DB_HOST', default = 'localhost') ,  # Ou l'adresse IP de votre serveur PostgreSQL
+        'PORT':  config('DB_PORT' , default = '5432') ,      # Le port par défaut pour PostgreSQL
     }
 }
+
 
 
 
